5647102a137a652cea618a62cc51fbbd121ec141
Simon Stucki <simon_stucki@hotmail.com>
2020-02-25 21:44:50 +0100

    Add Darwin Game Frontend Setup
    Bootstrap frontend for web application.

---
+++ game-frontend/src/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import logo from './logo.svg';
+
+const GlobalStyle = createGlobalStyle`
+ body {
+   margin: 0;
+   padding: 0;
+   font-family: sans-serif;
+ }
+`;
+
+const AppContainer = styled.div`
+ text-align: center;
+`;
+
+const AppHeader = styled.header`
+ background-color: #222;
+ height: 150px;
+ padding: 20px;
+ color: white;
+`;
+
+const AppTitle = styled.h1`
+   font-size: 1.5em;
+`;
+
+const AppLogo = styled.img`
+ animation: App-logo-spin infinite 20s linear;
+ height: 80px;
+ @keyframes App-logo-spin {
+   from { transform: rotate(0deg); }
+   to { transform: rotate(360deg); }
+ }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AppContainer>
+        <AppHeader>
+          <AppLogo src={logo} className="App-logo" alt="logo" />
+          <AppTitle>
+            Darwin is ready for React.
+          </AppTitle>
+        </AppHeader>
+      </AppContainer>
+    </>
+  );
+}
+
+export default App;
