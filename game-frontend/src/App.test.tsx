acb58b607034dca403b6837c92284e45c1dae14e
Simon Stucki <simon_stucki@hotmail.com>
2020-02-26 07:57:19 +0100

    Update unit test

--- game-frontend/src/App.test.tsx
+++ game-frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Darwin text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/Darwin/i);
+  expect(titleElement).toBeInTheDocument();
 });
