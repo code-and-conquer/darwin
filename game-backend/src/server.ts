3d99ea732304902ae9e86803e7739b1fb5ac64f0
Michael Schaufelberger <michael.schufi@gmail.com>
2020-02-27 22:57:58 +0100

    added websocket server

--- game-backend/src/server.ts
+++ game-backend/src/server.ts
@@ -1,1 +1,17 @@
-console.log('asdf');
+import http from 'http';
+import WebSocket from 'ws';
+
+const server = http.createServer();
+const WSServer = new WebSocket.Server({
+  server,
+});
+
+/* eslint-disable no-console */
+WSServer.on('connection', (ws) => {
+  console.log('asdfasdf');
+
+  ws.on('message', (message) => {
+    console.log(`received ${message}`);
+  });
+});
+/* eslint-enable no-console */
