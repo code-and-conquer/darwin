688cfaaa6cc29826b981eb59657f8205b60a9115
Michael Schaufelberger <michael.schufi@gmail.com>
2020-02-29 18:39:49 +0100

    removed outdated linting exclusions

--- game-backend/src/server.ts
+++ game-backend/src/server.ts
@@ -8,11 +8,9 @@ const WSServer = new WebSocket.Server({
   server,
 });
 
-/* eslint-disable no-console */
 WSServer.on('connection', (ws) => {
   MainController.newConnection(ws);
 });
-/* eslint-enable no-console */
 
 // start server
 server.listen(8080, '0.0.0.0');
