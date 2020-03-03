1fa11eded89cebb3aafc58d4c351c5a4c87cdb6d
Simon Stucki <simon_stucki@hotmail.com>
2020-03-03 09:21:37 +0100

    AB-36: Use dynamic backend URL (#10)
    Set .env files for changing backend url.

--- game-frontend/src/useWebsocketData.js
+++ game-frontend/src/useWebsocketData.js
@@ -1,9 +1,11 @@
 import {useEffect, useState} from 'react';
 
+const API_URL = process.env.REACT_APP_BACKEND_URL;
+
 function useWebsocketData() {
     const [gamePosition, setGamePosition] = useState(null);
     useEffect(() => {
-        const socket = new WebSocket('ws://localhost:8080');
+        const socket = new WebSocket(API_URL);
 
         socket.addEventListener('message', function (event) {
             setGamePosition(JSON.parse(event.data))
