d2c24ecb5e84a995e77e1193e33d61f5852150a1
Marc <marc.berli@windowslive.com>
2020-03-05 21:15:08 +0100

    Simplify square drawing

--- game-frontend/src/components/pixi/square.jsx
+++ game-frontend/src/components/pixi/square.jsx
@@ -2,23 +2,17 @@ import React from 'react'
 import { Graphics } from '@inlet/react-pixi';
 
 function Square({ size, position, color = 0xffffff, alpha = 1, lineWidth = 1 }) {
-  const { x: x1, y: y1 } = position
-  const x2 = x1 + size
-  const y2 = y1 + size
+  const { x, y } = position
   return (
     <Graphics
       draw={
         g => {
           // clear the graphics
           g.clear()
-          // start drawing
+
           g.lineStyle(lineWidth, color, alpha)
 
-          g.moveTo(x1, y1)
-          g.lineTo(x2, y1)
-          g.lineTo(x2, y2)
-          g.lineTo(x1, y2)
-          g.lineTo(x1, y1)
+          g.drawRect(x, y, size, size)
         }
       }
     />
