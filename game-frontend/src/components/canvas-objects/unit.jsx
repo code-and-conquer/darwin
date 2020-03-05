4410200d11d27f6b22aaf61d277a6f1e34d0361b
Marc <marc.berli@windowslive.com>
2020-03-05 21:09:52 +0100

    Add Unit component

---
+++ game-frontend/src/components/canvas-objects/unit.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Square from '../pixi/square'
+
+function Unit({ position }) {
+  return <Square position={position} size={5} color={0xffd900} />
+}
+
+export default Unit
