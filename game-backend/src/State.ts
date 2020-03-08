9d082226b15a9cba5b24fcc98e5110a6f56310a8
Marc <marc.berli@windowslive.com>
2020-03-08 09:24:45 +0100

    Add basic websocket session handling POC

---
+++ game-backend/src/State.ts
@@ -0,0 +1,10 @@
+import { State } from '../../darwin-types/State';
+
+const state: State = {
+  objectIds: [],
+  objectMap: {},
+  userContextIds: [],
+  userContextMap: {},
+}
+
+export default state
