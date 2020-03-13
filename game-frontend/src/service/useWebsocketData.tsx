1ad471345723086ba137ffbc48b0dcd06ef384ce
Marc <marc.berli@windowslive.com>
2020-03-13 16:32:04 +0100

    Use reducer for frontend state handling

--- game-frontend/src/service/useWebsocketData.tsx
+++ game-frontend/src/service/useWebsocketData.tsx
@@ -1,6 +1,16 @@
-import React, { useEffect, useState, createContext, useContext } from 'react';
+import React, { useEffect, createContext, useContext, useReducer } from 'react';
 import { MatchUpdate } from '../../../darwin-types/messages/MatchUpdate';
 import { State } from '../../../darwin-types/State';
+
+const reducer = (state: State, action: MatchUpdate): State => {
+  switch (action.type) {
+    case 'matchUpdate': {
+      return action.payload.state;
+    }
+    default:
+      return state;
+  }
+};
 
 const API_URL = process.env.REACT_APP_BACKEND_URL as string;
 const emptyState: State = { objectMap: {}, objectIds: [] };
@@ -8,17 +18,17 @@ const emptyState: State = { objectMap: {}, objectIds: [] };
 const WebsocketContext = createContext<State>(emptyState);
 
 function useGetWebsocketData(): State {
-  const [gameState, setGameState] = useState<State>(emptyState);
+  const [state, dispatch] = useReducer(reducer, emptyState);
   useEffect(() => {
     const socket = new WebSocket(API_URL);
 
     socket.addEventListener('message', event => {
-      const EventData: MatchUpdate = JSON.parse(event.data);
+      const action: MatchUpdate = JSON.parse(event.data);
 
-      setGameState(EventData.payload.state);
+      dispatch(action);
     });
   }, []);
-  return gameState;
+  return state;
 }
 
 export function WebsocketProvider(props: {
