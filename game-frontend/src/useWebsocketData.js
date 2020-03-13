7ecdab68b835e67566d2ca046199cd04135e0c30
Marc Berli <marc.berli@windowslive.com>
2020-03-13 10:44:57 +0100

    Readd missing linebreak

--- game-frontend/src/useWebsocketData.js
+++ game-frontend/src/useWebsocketData.js
@@ -6,6 +6,7 @@ function useWebsocketData() {
   const [gamePosition, setGamePosition] = useState(null);
   useEffect(() => {
     const socket = new WebSocket(API_URL);
+
     socket.addEventListener('message', function(event) {
       setGamePosition(JSON.parse(event.data));
     });
