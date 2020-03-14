8995ec039e4f52ef89870750132f2d5ea3cf8ca2
Simon Stucki <simon_stucki@hotmail.com>
2020-03-14 15:48:15 +0100

    Add basic textarea styling

--- game-frontend/src/components/visual/Textarea.tsx
+++ game-frontend/src/components/visual/Textarea.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
-const Textarea = styled.textarea``;
+const Textarea = styled.textarea`
+  background-color: #282a36;
+  color: #f8f8f2;
+  width: 100%;
+`;
 
 export default Textarea;
