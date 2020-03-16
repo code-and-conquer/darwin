f31652d45649cff1064d94bfc388f766d78f0244
Simon Stucki <simon_stucki@hotmail.com>
2020-03-16 19:09:47 +0100

    Extract Button component

--- game-frontend/src/App.tsx
+++ game-frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-
-import styled from 'styled-components';
 import logo from './logo.svg';
 import Game from './Game';
 import { WebsocketProvider } from './service/useWebsocketData';
@@ -8,22 +6,10 @@ import AppContainer from './components/container/AppContainer';
 import AppHeader from './components/visual/AppHeader';
 import AppTitle from './components/visual/AppTitle';
 import AppLogo from './components/visual/AppLogo';
+import Button from './components/visual/Button';
 import GlobalStyle from './GlobalStyle';
 import UserScript from './components/user-script';
-
 import HelpDialog from './components/HelpDialog';
-
-const Button = styled.button`
-  background-color: rebeccapurple;
-  border: none;
-  border-radius: 200px;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-`;
 
 function App(): JSX.Element {
   const [isHelpDialogOpen, setIsHelpDialogOpen] = useState(false);
@@ -34,8 +20,7 @@ function App(): JSX.Element {
         <AppHeader>
           {isHelpDialogOpen && <HelpDialog />}
           <Button onClick={() => setIsHelpDialogOpen(!isHelpDialogOpen)}>
-            {' '}
-            Help 
+            Help
           </Button>
           <AppLogo src={logo} className="App-logo" alt="logo" />
           <AppTitle>Darwin is ready for React.</AppTitle>
---
+++ game-frontend/src/components/visual/Button.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: rebeccapurple;
+  border: none;
+  border-radius: 200px;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+`;
+
+export default Button;
