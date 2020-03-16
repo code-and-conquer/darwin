c145a1d7cbdd4c69d8bf302832c4c1599450592f
Simon Stucki <simon_stucki@hotmail.com>
2020-03-16 19:08:21 +0100

    Apply prettier

--- game-frontend/src/components/HelpDialog.tsx
+++ game-frontend/src/components/HelpDialog.tsx
@@ -17,34 +17,62 @@ const Modal = styled.div`
 `;
 
 const HelpDialog: FC = () => {
-return <Modal>
-<h3>Ziel: Überleben!</h3>
-<h3>Der Spieler überlebt indem er:</h3>
-<ul>
-<li><p>Nahrung aufsammelt damit er nicht verhungert</p></li>
-<li><p>Ressourcen aufsammelt um Ausrüstung oder Gebäude zu bauen</p></li>
-<li><p>Gegnerische Einheiten angreift</p></li>
-</ul>
-<h3>Anleitung:</h3>
-<ul>
-<li><h4>Match ausschreiben:</h4></li>
-<li><p>blax Code eingeben.</p></li>
-</ul>
-<ul>
-<li><h4>Match beitreten:</h4></li>
-<li><p>xalb Code eingeben.</p></li>
-<li><h4>Match mitverfolgen:</h4></li>
-<li><p>axbl Code eingeben.</p></li>
-<li><h4>Match steuern mittels code:</h4></li>
-<li><p>lbxa Code eingeben.</p></li>
-<li><h4>Diese Hilfe anzeigen:</h4></li>
-<li><p>help eingeben.</p></li>
-</ul>
-<h3>Strategie Tipps:</h3>
-<ul>
-<li>Noch unbekannt.</li>
-</ul>
-</Modal>;
+  return (
+    <Modal>
+      <h3>Ziel: Überleben!</h3>
+      <h3>Der Spieler überlebt indem er:</h3>
+      <ul>
+        <li>
+          <p>Nahrung aufsammelt damit er nicht verhungert</p>
+        </li>
+        <li>
+          <p>Ressourcen aufsammelt um Ausrüstung oder Gebäude zu bauen</p>
+        </li>
+        <li>
+          <p>Gegnerische Einheiten angreift</p>
+        </li>
+      </ul>
+      <h3>Anleitung:</h3>
+      <ul>
+        <li>
+          <h4>Match ausschreiben:</h4>
+        </li>
+        <li>
+          <p>blax Code eingeben.</p>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h4>Match beitreten:</h4>
+        </li>
+        <li>
+          <p>xalb Code eingeben.</p>
+        </li>
+        <li>
+          <h4>Match mitverfolgen:</h4>
+        </li>
+        <li>
+          <p>axbl Code eingeben.</p>
+        </li>
+        <li>
+          <h4>Match steuern mittels code:</h4>
+        </li>
+        <li>
+          <p>lbxa Code eingeben.</p>
+        </li>
+        <li>
+          <h4>Diese Hilfe anzeigen:</h4>
+        </li>
+        <li>
+          <p>help eingeben.</p>
+        </li>
+      </ul>
+      <h3>Strategie Tipps:</h3>
+      <ul>
+        <li>Noch unbekannt.</li>
+      </ul>
+    </Modal>
+  );
 };
 
 export default HelpDialog;
