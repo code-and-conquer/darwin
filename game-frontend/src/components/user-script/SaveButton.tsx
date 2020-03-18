00263eaf40914519f43bc9f08e6ca674cb1cbc4d
Marc <marc.berli@windowslive.com>
2020-03-18 15:13:38 +0100

    Extract button component

--- game-frontend/src/components/user-script/SaveButton.tsx
+++ game-frontend/src/components/user-script/SaveButton.tsx
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
+import Button from '../visual/Button';
 
-const SaveButton = styled.button`
+const SaveButton = styled(Button)`
   position: absolute;
   right: 15px;
   bottom: 15px;
-  border: 0;
-  background: rgb(63, 81, 181);
-  color: #ffffff;
-  font-size: 16px;
-  padding: 10px 20px;
 `;
 
 export default SaveButton;
