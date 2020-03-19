e37d8e58d0dc5f2981ab3ab67732437a132e74f4
Marc <marc.berli@windowslive.com>
2020-03-19 19:02:12 +0100

    Add render layer order

--- game-frontend/src/components/canvas-objects/GameObjects.tsx
+++ game-frontend/src/components/canvas-objects/GameObjects.tsx
@@ -15,47 +15,49 @@ type Props = {
   scaleFactor: number;
 };
 
-const GameObjects: FC<Props> = ({
-  objectIds,
-  objectMap,
-  ownUnitId,
-  scaleFactor,
-}) => (
+const sortLayerConfig = {
+  [GAME_OBJECT_TYPES.UNIT]: 2,
+  [GAME_OBJECT_TYPES.FOOD]: 1,
+};
+
+const GameObjects: FC<Props> = ({ objectIds, objectMap, scaleFactor }) => (
   <>
-    {objectIds.map((objectId): JSX.Element | null => {
-      const gameObject = objectMap[objectId];
-      switch (gameObject.type) {
-        case GAME_OBJECT_TYPES.UNIT: {
-          const unit = gameObject as UnitT;
-          return (
-            <Unit
-              key={objectId}
-              health={unit.health}
-              position={{
-                x: unit.position.x * scaleFactor,
-                y: unit.position.y * scaleFactor,
-              }}
-              isOwn={objectId === ownUnitId}
-            />
-          );
+    {objectIds
+      .map(objectId => objectMap[objectId])
+      .sort((a, b) =>
+        sortLayerConfig[a.type] > sortLayerConfig[b.type] ? 1 : -1
+      )
+      .map((gameObject): JSX.Element | null => {
+        switch (gameObject.type) {
+          case GAME_OBJECT_TYPES.UNIT: {
+            const unit = gameObject as UnitT;
+            return (
+              <Unit
+                key={unit.id}
+                health={unit.health}
+                position={{
+                  x: unit.position.x * scaleFactor,
+                  y: unit.position.y * scaleFactor,
+                }}
+              />
+            );
+          }
+          case GAME_OBJECT_TYPES.FOOD: {
+            const food = gameObject as UnitT;
+            return (
+              <Food
+                key={food.id}
+                position={{
+                  x: food.position.x * scaleFactor,
+                  y: food.position.y * scaleFactor,
+                }}
+              />
+            );
+          }
+          default:
+            return null;
         }
-        case GAME_OBJECT_TYPES.FOOD: {
-          const food = gameObject as UnitT;
-          return (
-            <Food
-              key={objectId}
-              position={{
-                x: food.position.x * scaleFactor,
-                y: food.position.y * scaleFactor,
-              }}
-            />
-          );
-        }
-        default:
-          return null;
-      }
-    })}
+      })}
   </>
 );
-
 export default GameObjects;
