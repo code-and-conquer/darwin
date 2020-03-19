217261731900698dd4bdce6801c4288117e5162a
Marc <marc.berli@windowslive.com>
2020-03-19 15:59:00 +0100

    Add Food to gameObjects rendering

--- game-frontend/src/components/canvas-objects/GameObjects.tsx
+++ game-frontend/src/components/canvas-objects/GameObjects.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from 'react';
 import Unit from './Unit';
-import { ObjectId, GameObject } from '../../../../darwin-types/GameObject';
+import {
+  ObjectId,
+  GameObject,
+} from '../../../../darwin-types/game-objects/GameObject';
+import { Unit as UnitT } from '../../../../darwin-types/game-objects/Unit';
+import Food from './Food';
 
 type Props = {
   objectIds: ObjectId[];
@@ -10,17 +15,37 @@ type Props = {
 
 const GameObjects: FC<Props> = ({ objectIds, objectMap, scaleFactor }) => (
   <>
-    {objectIds.map(objectId => {
-      const { position } = objectMap[objectId];
-      return (
-        <Unit
-          key={objectId}
-          position={{
-            x: position.x * scaleFactor,
-            y: position.y * scaleFactor,
-          }}
-        />
-      );
+    {objectIds.map((objectId): JSX.Element | null => {
+      const gameObject = objectMap[objectId];
+      switch (gameObject.type) {
+        case 'unit': {
+          const unit = gameObject as UnitT;
+          return (
+            <Unit
+              key={objectId}
+              health={unit.health}
+              position={{
+                x: unit.position.x * scaleFactor,
+                y: unit.position.y * scaleFactor,
+              }}
+            />
+          );
+        }
+        case 'food': {
+          const food = gameObject as UnitT;
+          return (
+            <Food
+              key={objectId}
+              position={{
+                x: food.position.x * scaleFactor,
+                y: food.position.y * scaleFactor,
+              }}
+            />
+          );
+        }
+        default:
+          return null;
+      }
     })}
   </>
 );
