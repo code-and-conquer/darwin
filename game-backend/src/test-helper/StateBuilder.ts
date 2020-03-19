22a8b898ddc09921500218675533445d7cb9d34c
Marc <marc.berli@windowslive.com>
2020-03-19 16:54:21 +0100

    Refactor statebuilder test helper

--- game-backend/src/test-helper/StateBuilder.ts
+++ game-backend/src/test-helper/StateBuilder.ts
@@ -1,7 +1,10 @@
 import produce from 'immer';
 import { State } from '../../../darwin-types/State';
 
-export interface GameObject {
+export interface GameObject extends GameObjectWithoutType {
+  type: string;
+}
+export interface GameObjectWithoutType {
   id: string;
   x: number;
   y: number;
@@ -12,15 +15,24 @@ export default class StateBuilder {
     return this;
   }
 
-  addObject({ id, x, y }: GameObject): StateBuilder {
+  addObject({ id, x, y, type }: GameObject): StateBuilder {
     this.state = produce(this.state, draft => {
       draft.objectMap[id] = {
         id,
+        type,
         position: { x, y },
       };
       draft.objectIds.push(id);
     });
     return this;
+  }
+
+  addUnit({ id, x, y }: GameObjectWithoutType): StateBuilder {
+    return this.addObject({ id, x, y, type: 'unit' });
+  }
+
+  addFood({ id, x, y }: GameObjectWithoutType): StateBuilder {
+    return this.addObject({ id, x, y, type: 'food' });
   }
 
   build(): State {
