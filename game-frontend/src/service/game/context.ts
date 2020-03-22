3dccabfe9227887337ef60028fe6bebcd511ddd7
Simon Stucki <simon_stucki@hotmail.com>
2020-03-22 18:32:14 +0100

    Implement Persistence Frontend Handling
    Refactor context.ts

--- game-frontend/src/service/game/context.ts
+++ game-frontend/src/service/game/context.ts
@@ -1,4 +1,11 @@
-import { createContext, useContext, useEffect, useReducer } from 'react';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+} from 'react';
+import createPersistedState from 'use-persisted-state';
 import {
   API_URL,
   ContextState,
@@ -7,28 +14,57 @@ import {
 } from './types';
 import reducer from './reducer';
 import { Message } from '../../../../darwin-types/messages/Message';
+import { ConnectionInitialization } from '../../../../darwin-types/messages/ConnectionInitialization';
+import { UserId } from '../../../../darwin-types/UserContext';
+
+const USER_ID_QUERY_PARAM = 'userId';
+const useUserId = createPersistedState(USER_ID_QUERY_PARAM);
 
 export const WebsocketContext = createContext<ContextState>(
   emptyWebsocketContext
 );
 
 export function useWebsocket(): ContextState {
+  const [userId, setUserId] = useUserId<UserId | null>(null);
+  const selfObtainedUserIdRef = useRef<UserId | null>(null);
   const [contextState, dispatch] = useReducer(reducer, emptyWebsocketContext);
   const { socket } = contextState;
 
   useEffect(() => {
-    dispatch(socketUpdateAction(new WebSocket(API_URL)));
-  }, [dispatch]);
+    const hasNoActiveUserId = userId === null;
+    const hasObtainedUserIdInCurrentTab =
+      selfObtainedUserIdRef.current === userId;
+    if (hasNoActiveUserId || !hasObtainedUserIdInCurrentTab) {
+      const params = new URLSearchParams();
+      if (userId !== null) {
+        params.set(USER_ID_QUERY_PARAM, userId);
+      }
+      dispatch(
+        socketUpdateAction(new WebSocket(`${API_URL}?${params.toString()}`))
+      );
+    }
+  }, [userId, dispatch]);
 
   useEffect(() => {
+    const handleConnectionInitialization = (
+      message: ConnectionInitialization
+    ): void => {
+      selfObtainedUserIdRef.current = message.payload.userId;
+      setUserId(message.payload.userId);
+    };
     if (socket) {
       socket.addEventListener('message', event => {
         const action: Message = JSON.parse(event.data);
-
-        dispatch(action);
+        switch (action.type) {
+          case 'connectionInitialization':
+            handleConnectionInitialization(action as ConnectionInitialization);
+            break;
+          default:
+            dispatch(action);
+        }
       });
     }
-  }, [socket, dispatch]);
+  }, [socket, setUserId, dispatch]);
   return contextState;
 }
 
