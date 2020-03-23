241caffb131c37731b00acb0214889e62463dd23
Raphael Mailänder <mailarap@students.zhaw.ch>
2020-03-23 16:50:36 +0100

    move tests to gameObjects.test.ts

---
+++ game-backend/src/helper/gameObjects.test.ts
@@ -0,0 +1,81 @@
+import StateBuilder from '../test-helper/StateBuilder';
+import { State } from '../../../darwin-types/State';
+import { GAME_OBJECT_TYPES } from '../../../darwin-types/game-objects/GameObject';
+import { getGameObjectsPerType } from './gameObjects';
+
+describe('getGameObjectsPerType', () => {
+  const FOOD_ID = 'FOODID1';
+  const UNIT_ID = 'unit1';
+  const multipleObjectsCount = 5;
+  let emptyState: State;
+  let oneObjectOfEachState: State;
+  let multipleObjectState: State;
+  let multipleObjectStateBuilder: StateBuilder;
+
+  beforeEach(() => {
+    oneObjectOfEachState = StateBuilder.buildState()
+      .addFood({ id: FOOD_ID, x: 5, y: 17 })
+      .addUnit({ id: UNIT_ID, x: 2, y: 10 })
+      .build();
+
+    multipleObjectStateBuilder = StateBuilder.buildState();
+    for (let i = 1; i <= multipleObjectsCount; i++) {
+      multipleObjectStateBuilder.addFood({ id: `Food_${i}`, x: i, y: i });
+      multipleObjectStateBuilder.addUnit({
+        id: `Unit_${i}`,
+        x: i + 1,
+        y: i + 1,
+      });
+    }
+    multipleObjectState = multipleObjectStateBuilder.build();
+
+    emptyState = StateBuilder.buildState().build();
+  });
+
+  it('filters state with zero food and units', () => {
+    const foods = getGameObjectsPerType(emptyState, GAME_OBJECT_TYPES.FOOD);
+    const units = getGameObjectsPerType(emptyState, GAME_OBJECT_TYPES.UNIT);
+
+    expect(foods.length).toBe(0);
+    expect(units.length).toBe(0);
+  });
+
+  it('filters state with one food and unit', () => {
+    const foods = getGameObjectsPerType(
+      oneObjectOfEachState,
+      GAME_OBJECT_TYPES.FOOD
+    );
+    const units = getGameObjectsPerType(
+      oneObjectOfEachState,
+      GAME_OBJECT_TYPES.UNIT
+    );
+
+    expect(foods.length).toBe(1);
+    expect(foods[0].type).toBe(GAME_OBJECT_TYPES.FOOD);
+    expect(foods[0].position.x).toBe(5);
+    expect(foods[0].position.y).toBe(17);
+
+    expect(units.length).toBe(1);
+    expect(units[0].type).toBe(GAME_OBJECT_TYPES.UNIT);
+    expect(units[0].position.x).toBe(2);
+    expect(units[0].position.y).toBe(10);
+  });
+
+  it('filters state with multiple objects', () => {
+    const foods = getGameObjectsPerType(
+      multipleObjectState,
+      GAME_OBJECT_TYPES.FOOD
+    );
+    const units = getGameObjectsPerType(
+      multipleObjectState,
+      GAME_OBJECT_TYPES.UNIT
+    );
+
+    expect(foods.length).toBe(multipleObjectsCount);
+    expect(units.length).toBe(multipleObjectsCount);
+    for (let i = 0; i < multipleObjectsCount; i++) {
+      expect(foods[i].type).toBe(GAME_OBJECT_TYPES.FOOD);
+      expect(units[i].type).toBe(GAME_OBJECT_TYPES.UNIT);
+    }
+  });
+});
