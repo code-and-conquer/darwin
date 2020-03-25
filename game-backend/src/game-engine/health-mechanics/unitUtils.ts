abc132476e0f71454642db246936e125d0e4f333
ferinator <kunticferenc@gmail.com>
2020-03-25 21:42:55 +0100

    clean up

--- game-backend/src/game-engine/health-mechanics/unitUtils.ts
+++ game-backend/src/game-engine/health-mechanics/unitUtils.ts
@@ -1,5 +1,4 @@
 import { State } from '../../../../darwin-types/State';
-import produce from '../../helper/produce';
 import {
   ObjectId,
   GameObject,
@@ -8,13 +7,13 @@ import {
 import { Unit } from '../../../../darwin-types/game-objects/Unit';
 import { HEALTH_LOSS_RATE } from '../../constants/gameObjects';
 import { getGameObjectsPerType } from '../../helper/gameObjects';
+import produce from '../../helper/produce';
 
 export function updateHealth(state: State) {
-  const newState = produce(state, draft => {
+  return produce(state, draft => {
     const units = getGameObjectsPerType(state, GAME_OBJECT_TYPES.UNIT);
     units.forEach((unit: Unit) => {
       unit.health *= HEALTH_LOSS_RATE;
     });
   });
-  return newState;
 }
