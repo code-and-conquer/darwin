e94caa8cb55cbf825da7df2f8697ef61d9da7f72
Marc <marc.berli@windowslive.com>
2020-03-26 11:55:14 +0100

    Extract fields helper functions

--- game-backend/src/helper/fields.ts
+++ game-backend/src/helper/fields.ts
@@ -2,8 +2,8 @@ import { State } from '../../../darwin-types/State';
 import Position from '../../../darwin-types/Position';
 import { ARENA_WIDTH, ARENA_HEIGHT } from '../../../darwin-types/Arena';
 
-export const getFreeFields = (state: State): Position[] => {
-  const fields: Position[] = new Array(ARENA_WIDTH)
+export const getFlatFieldArray = (): Position[] =>
+  new Array(ARENA_WIDTH)
     .fill(undefined)
     .reduce(
       (acc, _, i) => [
@@ -14,16 +14,19 @@ export const getFreeFields = (state: State): Position[] => {
       ],
       []
     );
-  const occupiedFields: Record<string, boolean> = state.objectIds.reduce(
-    (acc, id) => {
-      const pos = state.objectMap[id].position;
-      return {
-        ...acc,
-        [`${pos.x}:${pos.y}`]: true,
-      };
-    },
-    {}
-  );
+
+export const getOccupiedFieldMap = (state: State): Record<string, boolean> =>
+  state.objectIds.reduce((acc, id) => {
+    const pos = state.objectMap[id].position;
+    return {
+      ...acc,
+      [`${pos.x}:${pos.y}`]: true,
+    };
+  }, {});
+
+export const getFreeFields = (state: State): Position[] => {
+  const fields = getFlatFieldArray();
+  const occupiedFields = getOccupiedFieldMap(state);
   const freeFields = fields.filter(
     field => !occupiedFields[`${field.x}:${field.y}`]
   );
