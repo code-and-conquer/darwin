74b20135819c382b95e6f006b6106aa54208ae31
Marc <marc.berli@windowslive.com>
2020-03-27 14:35:05 +0100

    Refactor consume intent

--- game-backend/src/game-engine/intent/ConsumeIntent.ts
+++ game-backend/src/game-engine/intent/ConsumeIntent.ts
@@ -8,17 +8,17 @@ import { GAME_OBJECT_TYPES } from '../../../../darwin-types/game-objects/GameObj
 import { Food } from '../../../../darwin-types/game-objects/Food';
 
 export const FOOD_REGENERATION_VALUE = 20;
-/* eslint class-methods-use-this: off */
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["execute"] }] */
 
 /**
  * Represents the intent to consume a food object.
  */
 export default class ConsumeIntent implements Intent {
-  private heal(health: number): number {
+  private static heal(health: number): number {
     return Math.min(health + FOOD_REGENERATION_VALUE, MAX_HEALTH);
   }
 
-  private getConsumableInReach(state: State, unit: Unit): Food | null {
+  private static getConsumableInReach(state: State, unit: Unit): Food | null {
     const objectsOnPosition = getObjectsOnField(state, unit.position);
     const consumable = objectsOnPosition
       ? objectsOnPosition.find(obj => obj.type === GAME_OBJECT_TYPES.FOOD)
@@ -29,11 +29,11 @@ export default class ConsumeIntent implements Intent {
   execute(state: State, userContext: UserContext): State {
     return produce(state, draft => {
       const unit = draft.objectMap[userContext.unitId] as Unit;
-      const consumable = this.getConsumableInReach(state, unit);
+      const consumable = ConsumeIntent.getConsumableInReach(state, unit);
       const canConsume = !!consumable;
 
       if (canConsume) {
-        unit.health = this.heal(unit.health);
+        unit.health = ConsumeIntent.heal(unit.health);
         delete draft.objectMap[consumable.id];
         draft.objectIds = draft.objectIds.filter(id => id !== consumable.id);
       }
