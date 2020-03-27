8f0c5235572c0b36597c9d78415fc38a87ce15d6
Marc <marc.berli@windowslive.com>
2020-03-27 14:45:30 +0100

    Refactor consume intent tests

--- game-backend/src/game-engine/intent/ConsumeIntent.test.ts
+++ game-backend/src/game-engine/intent/ConsumeIntent.test.ts
@@ -13,13 +13,13 @@ describe('ConsumeIntent', () => {
   const foodId2 = 'FOOD_ID_2';
 
   let state: State;
-  let userContext1: UserContext;
-  let userContext2: UserContext;
-  let userContext3: UserContext;
+  let lowHealth: UserContext;
+  let overflowHealth: UserContext;
+  let noReachableFood: UserContext;
 
-  let originalUnit1: Unit;
-  let originalUnit2: Unit;
-  let originalUnit3: Unit;
+  let lowHealthUnit: Unit;
+  let overflowHealthUnit: Unit;
+  let noReachableFoodUnit: Unit;
 
   beforeEach(() => {
     state = StateBuilder.buildState()
@@ -30,17 +30,17 @@ describe('ConsumeIntent', () => {
       .addUnit({ id: unitId3, x: 5, y: 5, health: 20 })
       .build();
 
-    originalUnit1 = getUnit(state, unitId1);
-    originalUnit2 = getUnit(state, unitId2);
-    originalUnit3 = getUnit(state, unitId3);
+    lowHealthUnit = getUnit(state, unitId1);
+    overflowHealthUnit = getUnit(state, unitId2);
+    noReachableFoodUnit = getUnit(state, unitId3);
 
-    userContext1 = {
+    lowHealth = {
       unitId: unitId1,
     };
-    userContext2 = {
+    overflowHealth = {
       unitId: unitId2,
     };
-    userContext3 = {
+    noReachableFood = {
       unitId: unitId3,
     };
   });
@@ -48,15 +48,15 @@ describe('ConsumeIntent', () => {
   it('handles consume properly', () => {
     const intent = new ConsumeIntent();
 
-    const newState = intent.execute(state, userContext1);
+    const newState = intent.execute(state, lowHealth);
     const unit1 = getUnit(state, unitId1);
     const unit2 = getUnit(state, unitId2);
 
     const food1 = getFood(state, foodId1);
     const food2 = getFood(state, foodId2);
 
-    expect(unit1.health).toBe(originalUnit1.health + FOOD_REGENERATION_VALUE);
-    expect(unit2.health).toBe(originalUnit2.health);
+    expect(unit1.health).toBe(lowHealthUnit.health + FOOD_REGENERATION_VALUE);
+    expect(unit2.health).toBe(overflowHealthUnit.health);
     expect(food1).toBeFalsy();
     expect(food2).toBeTruthy();
     expect(newState.objectIds).not.toContain(foodId1);
@@ -66,23 +66,18 @@ describe('ConsumeIntent', () => {
   it('respects max health value', () => {
     const intent = new ConsumeIntent();
 
-    const newState = intent.execute(state, userContext2);
+    const newState = intent.execute(state, overflowHealth);
     const unit2 = getUnit(newState, unitId2);
 
-    const food1 = getFood(newState, foodId1);
-    const food2 = getFood(newState, foodId2);
-
     expect(unit2.health).toBe(MAX_HEALTH);
-    expect(food1).toBeTruthy();
-    expect(food2).toBeFalsy();
   });
 
   it('does not heal if no food is in reach', () => {
     const intent = new ConsumeIntent();
 
-    const newState = intent.execute(state, userContext3);
+    const newState = intent.execute(state, noReachableFood);
     const unit3 = getUnit(newState, unitId3);
 
-    expect(unit3.health).toBe(originalUnit3.health);
+    expect(unit3.health).toBe(noReachableFoodUnit.health);
   });
 });
