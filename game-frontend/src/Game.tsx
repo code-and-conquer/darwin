94d65d6b7318c3edf1675d273a5c4ed1520b7ef6
Simon Stucki <simon_stucki@hotmail.com>
2020-03-29 12:03:27 +0200

    AB#77 Show user death state (#64)
    Use filter as death indicator

--- game-frontend/src/Game.tsx
+++ game-frontend/src/Game.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
-import { Stage } from '@inlet/react-pixi';
+import React, { useEffect, useState } from 'react';
+import { Container, Stage } from '@inlet/react-pixi';
+import * as PIXI from 'pixi.js';
 import CanvasWrapper from './components/visual/CanvasWrapper';
 import { useGameState, useUserContext } from './service/game';
 import GameObjects from './components/canvas-objects/GameObjects';
 import Grid from './components/canvas-objects/Grid';
 import { FIELD_SIZE, STAGE_COLUMNS, STAGE_ROWS } from './constants/stage';
+import Rectangle from './components/pixi/Rectangle';
+
+const width = STAGE_ROWS * FIELD_SIZE;
+const height = STAGE_COLUMNS * FIELD_SIZE;
 
 function Game(): JSX.Element {
+  const [hasJoinedGame, setHasJoinedGame] = useState(false);
   const gameState = useGameState();
   const userContext = useUserContext();
 
-  if (!gameState) {
-    return <p>Loading</p>;
+  const isLiving = !!gameState.objectMap[userContext.unitId];
+
+  useEffect(() => {
+    if (isLiving) {
+      setHasJoinedGame(true);
+    }
+  }, [isLiving]);
+
+  const isDead = hasJoinedGame && !isLiving;
+
+  if (!hasJoinedGame) {
+    return <p>Warten...</p>;
+  }
+
+  const colorMatrix = new PIXI.filters.ColorMatrixFilter();
+  if (isDead) {
+    colorMatrix.sepia(true);
   }
 
   return (
     <CanvasWrapper>
       <Stage
         options={{
-          backgroundColor: 0x001437,
           resolution: window.devicePixelRatio,
         }}
-        width={STAGE_ROWS * FIELD_SIZE}
-        height={STAGE_COLUMNS * FIELD_SIZE}
+        width={width}
+        height={height}
       >
-        <Grid
-          numberOfRows={STAGE_ROWS}
-          numberOfColumns={STAGE_COLUMNS}
-          scaleFactor={FIELD_SIZE}
-        />
-        <GameObjects
-          objectIds={gameState.objectIds}
-          objectMap={gameState.objectMap}
-          ownUnitId={userContext.unitId}
-          scaleFactor={FIELD_SIZE}
-        />
+        <Container filters={isDead ? [colorMatrix] : []}>
+          <Rectangle
+            fill={0x001437}
+            width={width}
+            height={height}
+            position={{ x: 0, y: 0 }}
+          />
+          <Grid
+            numberOfRows={STAGE_ROWS}
+            numberOfColumns={STAGE_COLUMNS}
+            scaleFactor={FIELD_SIZE}
+          />
+          <GameObjects
+            objectIds={gameState.objectIds}
+            objectMap={gameState.objectMap}
+            ownUnitId={userContext.unitId}
+            scaleFactor={FIELD_SIZE}
+          />
+        </Container>
       </Stage>
     </CanvasWrapper>
   );
