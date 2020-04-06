6e4d73e605751a09bc4451dbbef52644818339d3
Marc <marc.berli@windowslive.com>
2020-04-06 22:33:24 +0200

    Extend powerup helpers

--- game-backend/src/helper/powerup.ts
+++ game-backend/src/helper/powerup.ts
@@ -1,12 +1,19 @@
-import { State, Consumable, GameObjectTypes, Consume } from '@darwin/types';
+import {
+  State,
+  Consumable,
+  GameObjectTypes,
+  Consume,
+  GameObject,
+} from '@darwin/types';
 import powerupMap from '../game-engine/mechanics/powerup-spawner/powerups/index';
 
+export const filterPowerups = (objects: GameObject[]): Consumable[] =>
+  objects.filter(
+    obj => !!(powerupMap as Record<GameObjectTypes, Consume>)[obj.type]
+  ) as Consumable[];
+
 export const getPowerups = (state: State): Consumable[] =>
-  state.objectIds
-    .map(id => state.objectMap[id])
-    .filter(
-      obj => !!(powerupMap as Record<GameObjectTypes, Consume>)[obj.type]
-    ) as Consumable[];
+  filterPowerups(state.objectIds.map(id => state.objectMap[id]));
 
 export const countPowerups = (state: State): number =>
   getPowerups(state).length;
