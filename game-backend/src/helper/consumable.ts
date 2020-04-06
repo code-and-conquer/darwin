74246f47669a68a9c59dd96730051cb9bdeb741a
Marc <marc.berli@windowslive.com>
2020-04-06 21:23:21 +0200

    Improve consumable helper

--- game-backend/src/helper/consumable.ts
+++ game-backend/src/helper/consumable.ts
@@ -1,8 +1,18 @@
-import { GameObjectTypes, GameObject, Consumable } from '@darwin/types';
+import {
+  GameObjectTypes,
+  GameObject,
+  Consumable,
+  ObjectId,
+  State,
+} from '@darwin/types';
 import { powerupList } from '../game-engine/mechanics/powerup-spawner/powerups/index';
+import { getObjectById } from './gameObjects';
 
 export const isConsumable = (gameObject: GameObject): boolean =>
   [GameObjectTypes.Food, ...powerupList].indexOf(gameObject.type) >= 0;
 
 export const filterConsumables = (gameObjects: GameObject[]): Consumable[] =>
   gameObjects.filter(isConsumable) as Consumable[];
+
+export const getConsumable = (state: State, id: ObjectId): Consumable =>
+  getObjectById<Consumable>(state, id);
