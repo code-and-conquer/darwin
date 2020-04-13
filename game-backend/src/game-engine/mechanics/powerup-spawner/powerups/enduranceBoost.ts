3e0be70e247a3c90f3ec6132c346a69403d43273
Marc Berli <marc.berli@windowslive.com>
2020-04-13 16:32:02 +0200

    Extract attribute powerup helper (#86)
    Co-authored-by: mailaenderli <61315599+mailaenderli@users.noreply.github.com>

--- game-backend/src/game-engine/mechanics/powerup-spawner/powerups/enduranceBoost.ts
+++ game-backend/src/game-engine/mechanics/powerup-spawner/powerups/enduranceBoost.ts
@@ -1,25 +1,9 @@
 import { Consume, AttributeName } from '@darwin/types';
-import produce from '../../../../helper/produce';
-import { getUnit, removeGameObject } from '../../../../helper/gameObjects';
-import { updateAttribute } from '../../../../helper/attribute';
+import createAttributePowerupConsumer from './createAttributePowerupConsumer';
 
-const consumeEnduranceBoost: Consume = (id, state, userContext) => {
-  return produce(state, draft => {
-    const unit = getUnit(draft, userContext.unitId);
-    const { value, hasChanged } = updateAttribute(
-      unit,
-      AttributeName.EnduranceBoost,
-      currentBoost => currentBoost + 1
-    );
-
-    if (hasChanged) {
-      unit.attributes.enduranceBoost = value;
-
-      const { objectIds, objectMap } = removeGameObject(draft, id);
-      draft.objectMap = objectMap;
-      draft.objectIds = objectIds;
-    }
-  });
-};
+const consumeEnduranceBoost: Consume = createAttributePowerupConsumer(
+  AttributeName.EnduranceBoost,
+  currentBoost => currentBoost + 1
+);
 
 export default consumeEnduranceBoost;
