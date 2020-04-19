c097277f1bf2cea081fc713a238f9ba9dd24a82d
Michael Schaufelberger <michael.schufi@gmail.com>
2020-04-19 16:14:50 +0200

    cleanup

--- game-frontend/src/components/help/index.tsx
+++ game-frontend/src/components/help/index.tsx
@@ -1,13 +1,8 @@
 import React, { FC } from 'react';
-// import useDialogState from '../../hooks/useDialogState';
-// import Button from '../visual/Button';
 import Container from './Container';
 import HelpText from './HelpText';
-// import DialogWrapper from '../dialog/DialogWrapper';
-// import Dialog from '../dialog';
 
 const Help: FC = () => {
-  // const [isOpen, open, close] = useDialogState();
   return (
     <>
       <Container>
