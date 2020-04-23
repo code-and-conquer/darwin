4349f0153bc104635eddd01e7cf90f106c45000c
Michael Schaufelberger <michael.schufi@gmail.com>
2020-04-23 11:56:23 +0200

    removed useEffect in ErrorLog

--- game-frontend/src/components/user-script/ErrorLog.tsx
+++ game-frontend/src/components/user-script/ErrorLog.tsx
@@ -1,15 +1,9 @@
-import React, { FC, useState, useEffect } from 'react';
-import { Hook, Console, Decode } from 'console-feed';
+import React, { FC, useState } from 'react';
+import { Console } from 'console-feed';
 import { Message } from 'console-feed/lib/definitions/Console';
 
 const ErrorLog: FC = () => {
-  const [logs, setLogs] = useState<Message[]>([]);
-
-  useEffect(() => {
-    Hook(window.console, log => {
-      setLogs([...logs, Decode(log)]);
-    });
-  });
+  const [logs] = useState<Message[]>([]);
 
   return (
     <div style={{ backgroundColor: '#242424' }}>
