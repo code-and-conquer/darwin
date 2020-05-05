0a1cd740d18cc30f46dbfb2a102d1d436bacddaf
Marc <marc.berli@windowslive.com>
2020-05-05 16:22:22 +0200

    Remove corners from map

--- game-backend/src/map/maps/feastOrFamine.ts
+++ game-backend/src/map/maps/feastOrFamine.ts
@@ -2,32 +2,32 @@ import { Position } from '@darwin/types';
 
 const warpedArena: Position[] = [
   // top left
+  { x: 6, y: 3 },
   { x: 6, y: 4 },
-  { x: 6, y: 5 },
-  { x: 6, y: 6 },
-  { x: 5, y: 6 },
+
+  { x: 3, y: 6 },
   { x: 4, y: 6 },
 
   // top right
-  { x: 4, y: 13 },
-  { x: 5, y: 13 },
-  { x: 6, y: 13 },
-  { x: 6, y: 14 },
-  { x: 6, y: 15 },
+  { x: 13, y: 3 },
+  { x: 13, y: 4 },
+
+  { x: 15, y: 6 },
+  { x: 16, y: 6 },
 
   // bottom left
-  { x: 13, y: 4 },
-  { x: 13, y: 5 },
-  { x: 13, y: 6 },
-  { x: 14, y: 6 },
-  { x: 15, y: 6 },
+  { x: 3, y: 13 },
+  { x: 4, y: 13 },
+
+  { x: 6, y: 15 },
+  { x: 6, y: 16 },
 
   // bottom right
-  { x: 14, y: 13 },
   { x: 15, y: 13 },
-  { x: 13, y: 13 },
-  { x: 13, y: 14 },
+  { x: 16, y: 13 },
+
   { x: 13, y: 15 },
+  { x: 13, y: 16 },
 
   // core
   { x: 8, y: 8 },
