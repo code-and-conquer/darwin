e72b806279c4cabd57e147363f68a882f97bebad
Marc <marc.berli@windowslive.com>
2020-05-05 16:16:43 +0200

    Add helptext section about the walls

--- game-frontend/src/components/help/HelpText.tsx
+++ game-frontend/src/components/help/HelpText.tsx
@@ -71,6 +71,13 @@ const HelpText: FC = () => {
         <InlineCode>'LEFT'</InlineCode>) zur Verfügung.
       </p>
       <Codeblock>move('UP')</Codeblock>
+      <h5>Wände</h5>
+      <p>
+        Es gibt Wände, die dir und deinen Gegner Schutz geben können, aber auch
+        das finden von Resourcen umständlicher gestaltet. Alle Wände auf der Map
+        findest du in der <InlineCode>walls</InlineCode> Variable. Damit kannst
+        du diese effektiv umkurven.
+      </p>
       <h4>
         Ressourcen konsumieren - <CostEmoji /> 3
       </h4>
