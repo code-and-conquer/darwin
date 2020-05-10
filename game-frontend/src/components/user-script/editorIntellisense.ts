1c4a5d192d00357627d43cb80a837f2fb2107771
flipmess <flipmess@gmail.com>
2020-05-10 18:08:02 +0200

    remove noLib: true so array types work

--- game-frontend/src/components/user-script/editorIntellisense.ts
+++ game-frontend/src/components/user-script/editorIntellisense.ts
@@ -1,59 +1,64 @@
 export default `
-enum Direction {
-    Up = 'UP',
-    Down = 'DOWN',
-    Left = 'LEFT',
-    Right = 'RIGHT',
-  };
-interface GameObject {
-    id: ObjectId;
-    type: GameObjectTypes;
-    position: Position;
-    moveBlocking: boolean;
-  };
-interface Unit extends GameObject {
-    type: GameObjectTypes.Unit;
-    health: number;
-    attributes: Attributes;
-  };
+declare enum Direction {
+  Up = 'UP',
+  Down = 'DOWN',
+  Left = 'LEFT',
+  Right = 'RIGHT',
+}
+declare type ObjectId = string;
+
+enum GameObjectTypes {
+  Unit = 'unit',
+  Food = 'food',
+  Wall = 'wall',
+  EnduranceBoost = 'enduranceBoost',
+  Teleport = 'teleport',
+  HealthRegenBoost = 'healthRegenBoost',
+}
+interface Position {
+  x: number;
+  y: number;
+}
+declare interface GameObject {
+  id: ObjectId;
+  type: GameObjectTypes;
+  position: Position;
+  moveBlocking: boolean;
+}
 
 enum AttributeName {
-    EnduranceBoost = 'enduranceBoost',
-    HealthRegenBoost = 'healthRegenBoost',
-  };
-
+  EnduranceBoost = 'enduranceBoost',
+  HealthRegenBoost = 'healthRegenBoost',
+}
 type Attributes = Record<AttributeName, number>;
+interface Unit extends GameObject {
+  type: GameObjectTypes.Unit;
+  health: number;
+  attributes: Attributes;
+}
+
+declare type ConsumableType = GameObjectTypes.Food | PowerupType;
 
 interface Consumable extends GameObject {
-    type: ConsumableType;
-  };
-
-type ConsumableType = GameObjectTypes.Food | PowerupType;
+  type: ConsumableType;
+}
 
 type PowerupType =
   | GameObjectTypes.EnduranceBoost
   | GameObjectTypes.Teleport
   | GameObjectTypes.HealthRegenBoost;
 
-enum GameObjectTypes {
-    Unit = 'unit',
-    Food = 'food',
-    EnduranceBoost = 'enduranceBoost',
-    Teleport = 'teleport',
-    HealthRegenBoost = 'healthRegenBoost',
-  };
-
-declare function move(direction: Direction): void;
-declare function consume(): void;
-declare function attack(unit: Unit = nearestEnemyUnit): void;
-
-declare const store: any;
+declare const store: Record<string, unknown>;
 declare const foods: Consumable[];
 declare const walls: GameObject[];
-declare const userUnit: Unit;
 declare const nearestFood: Consumable;
+declare const userUnit: Unit;
 declare const enemyUnits: Unit[];
 declare const nearestEnemyUnit: Unit;
 declare const powerups: Consumable[];
 declare const nearestPowerup: Consumable;
+
+declare function move(direction: Direction): void;
+declare function attack(unit: Unit = nearestEnemyUnit): void;
+declare function consume(): void;
 `;
