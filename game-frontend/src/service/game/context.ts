c9aec63174801f234c3fb91477cec81feea4918e
Michael Schaufelberger <michael.schufi@gmail.com>
2020-05-10 22:43:57 +0200

    persist role choice

--- game-frontend/src/service/game/context.ts
+++ game-frontend/src/service/game/context.ts
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useReducer } from 'react';
 import createPersistedState from 'use-persisted-state';
 import { v4 as uuidv4 } from 'uuid';
-import { Message, UserId } from '@darwin/types';
+import { Message, UserId, Role, RoleResponse } from '@darwin/types';
 import {
   API_URL,
   ContextState,
@@ -11,7 +11,9 @@ import {
 import reducer from './reducer';
 
 const USER_ID_QUERY_PARAM = 'userId';
+export const ROLE_LOCAL_STORAGE_KEY = 'role';
 const useUserId = createPersistedState(USER_ID_QUERY_PARAM);
+const usePersistRole = createPersistedState(ROLE_LOCAL_STORAGE_KEY);
 
 export const WebsocketContext = createContext<ContextState>(
   emptyWebsocketContext
@@ -19,6 +21,7 @@ export const WebsocketContext = createContext<ContextState>(
 
 export function useWebsocket(): ContextState {
   const [userId, setUserId] = useUserId<UserId>(() => uuidv4());
+  const [role, setRole] = usePersistRole<Role | null>(() => null);
   const [contextState, dispatch] = useReducer(reducer, emptyWebsocketContext);
   const { socket } = contextState;
 
@@ -34,11 +37,30 @@ export function useWebsocket(): ContextState {
   useEffect(() => {
     if (socket) {
       socket.addEventListener('message', event => {
-        const action: Message = JSON.parse(event.data);
-        dispatch(action);
+        const message: Message = JSON.parse(event.data);
+        dispatch(message);
+      });
+      // persist role choice
+      socket.addEventListener('message', event => {
+        const message: Message = JSON.parse(event.data);
+        if (message.type === 'roleResponse') {
+          const { newRole } = (message as RoleResponse).payload;
+          setRole(newRole);
+        }
       });
     }
-  }, [socket, setUserId, dispatch]);
+  }, [socket, setUserId, dispatch, setRole]);
+
+  // set initial role from localStorage
+  useEffect(() => {
+    if (role) {
+      const message: RoleResponse = {
+        type: 'roleResponse',
+        payload: { newRole: role },
+      };
+      dispatch(message);
+    }
+  });
   return contextState;
 }
 
