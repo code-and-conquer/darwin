2cc8dcb3d21c94a5e6f065555d8bd04540f156c2
Marc <marc.berli@windowslive.com>
2020-05-12 15:41:49 +0200

    Restore Food rendering

--- game-frontend/src/components/canvas-objects/Food.tsx
+++ game-frontend/src/components/canvas-objects/Food.tsx
@@ -1,21 +1,35 @@
 import React, { FC } from 'react';
 import { Position } from '@darwin/types';
-import { Sprite } from '@inlet/react-pixi';
+import Circle from '../pixi/Circle';
 import { FIELD_SIZE } from '../../constants/stage';
-import food from '../../assets/images/food.png';
 
 type Props = {
   position: Position;
 };
 
+const HALF_FIELD_SIZE = FIELD_SIZE / 2;
+const radius = HALF_FIELD_SIZE / 2;
+
 const Food: FC<Props> = ({ position }) => (
   <>
-    <Sprite
-      height={FIELD_SIZE}
-      width={FIELD_SIZE}
-      image={food}
-      x={position.x}
-      y={position.y}
+    <Circle
+      position={{
+        x: position.x + HALF_FIELD_SIZE,
+        y: position.y + HALF_FIELD_SIZE,
+      }}
+      radius={radius}
+      color={0x5aa1ff}
+      fill={0x5aa1ff}
+    />
+    <Circle
+      position={{
+        x: position.x + HALF_FIELD_SIZE,
+        y: position.y + HALF_FIELD_SIZE,
+      }}
+      radius={radius - 2}
+      lineWidth={0}
+      fill={0x000000}
+      alpha={0.2}
     />
   </>
 );
